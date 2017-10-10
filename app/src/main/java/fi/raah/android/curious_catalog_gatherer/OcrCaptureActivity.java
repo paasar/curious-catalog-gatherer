1c5e23eb20781eec876841f589d5bc0491ab40ae
Ari Paasonen <paasar@gmail.com>
2017-10-10 08:33:11 +0300

    Bootstrapping menu

--- app/src/main/java/fi/raah/android/curious_catalog_gatherer/OcrCaptureActivity.java
+++ app/src/main/java/fi/raah/android/curious_catalog_gatherer/OcrCaptureActivity.java
@@ -25,35 +25,29 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
 import fi.raah.android.curious_catalog_gatherer.ui.camera.CameraSource;
 import fi.raah.android.curious_catalog_gatherer.ui.camera.CameraSourcePreview;
 import fi.raah.android.curious_catalog_gatherer.ui.camera.GraphicOverlay;
-
-import com.google.android.gms.vision.text.TextRecognizer;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Activity for the Ocr Detecting app.  This app detects text and displays the value with the
@@ -81,25 +75,26 @@ public final class OcrCaptureActivity extends AppCompatActivity {
     // Helper objects for detecting taps and pinches.
     private GestureDetector gestureDetector;
 
-    // A TextToSpeech engine for speaking a String value.
-    private TextToSpeech tts;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-    //TODO refactor out of here
-    private HashMap<String, Set<String>> allCards = new HashMap<>();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    private Set<String> cardNames(AssetManager assetManager, String fileName) throws IOException {
-        InputStream inputStream = assetManager.open("cards/" + fileName);
-        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-        String line = null;
-        HashSet<String> cardNames = new HashSet<>();
-        while((line = in.readLine()) != null) {
-            cardNames.add(line);
-            //for (String cardName : cardNames) {
-            //    Log.d("CCG", "CARD NAME: " + cardName);
-            //}
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_owners_history) {
+            return true;
         }
 
-        return cardNames;
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -117,17 +112,6 @@ public final class OcrCaptureActivity extends AppCompatActivity {
         boolean autoFocus = true;
         boolean useFlash = false;
 
-//        AssetManager assetManager = getAssets();
-//        try {
-//            String[] assets = assetManager.list("cards");
-//            Log.d("CCG", "Assets: " + assets[0]);
-//
-//            for (String asset : assets) {
-//                allCards.put(asset, cardNames(assetManager, asset));
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         // Check for the camera permission before accessing the camera.  If the
         // permission is not granted yet, request permission.
         int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
